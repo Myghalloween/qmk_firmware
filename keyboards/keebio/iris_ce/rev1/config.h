a3e1e58320d347af14bd05d79a2acb540c46e005
FlamingPumpkin <82876933+Myghalloween@users.noreply.github.com>
2024-07-24 13:26:53 +0200

    Update config.h

--- keyboards/keebio/iris_ce/rev1/config.h
+++ keyboards/keebio/iris_ce/rev1/config.h
@@ -42,7 +42,7 @@ Additional RGB matrix option
 // #define RGB_MATRIX_DEFAULT_ON true 							// Sets the default enabled state, if none has been set
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR 			// Sets the default mode, if none has been set
 #define RGB_MATRIX_DEFAULT_HUE 172 								// Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 128 								// Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 112 								// Sets the default saturation value, if none has been set
 // #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
 // #define RGB_MATRIX_DEFAULT_SPD 127 							// Sets the default animation speed, if none has been set
 #define RGB_MATRIX_HUE_STEP 5 									// The number of steps to cycle through the hue by (default = 8) 
